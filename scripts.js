3d6b3881b84e81ba75e0992940f85ec1558a3264
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2025-01-30 17:04:35 -0800

    implement js for localStorage sets and gets along with editing, saving, and deleting notes;

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 const newNoteBtn = document.querySelector(".add-btn");
 
-const addNote = () => {
+const storedNotes = JSON.parse(localStorage.getItem("notes"));
+
+const addNote = (text = "") => {
 	const note = document.createElement("article");
 	note.classList.add("note");
 
@@ -9,17 +11,61 @@ const addNote = () => {
 			<button class="edit-btn">
 				<i class="fa-solid fa-pen-to-square"></i>
 			</button>
+            <button class="save-btn hidden">
+                <i class="fa-solid fa-floppy-disk"></i>
+            </button>
 			<button class="delete-btn">
 				<i class="fa-solid fa-trash-can"></i>
 			</button>
 		</div>
 		<p class="note-text">
-			New Note
+			${text ? `${text}` : "New Note"}
 		</p>
 		<textarea class="note-edit hidden"></textarea>
         `;
 
+	const editBtn = note.querySelector(".edit-btn");
+	const saveBtn = note.querySelector(".save-btn");
+	const deleteBtn = note.querySelector(".delete-btn");
+	const noteText = note.querySelector(".note-text");
+	const textarea = note.querySelector("textarea");
+
+	editBtn.addEventListener("click", () => {
+		editBtn.classList.toggle("hidden");
+		saveBtn.classList.toggle("hidden");
+		noteText.classList.toggle("hidden");
+		textarea.classList.toggle("hidden");
+		textarea.value = noteText.textContent.trim();
+	});
+
+	saveBtn.addEventListener("click", () => {
+		editBtn.classList.toggle("hidden");
+		saveBtn.classList.toggle("hidden");
+		noteText.classList.toggle("hidden");
+		textarea.classList.toggle("hidden");
+		noteText.textContent = textarea.value;
+		updateLocalStorage();
+	});
+
+	deleteBtn.addEventListener("click", () => {
+		note.remove();
+		updateLocalStorage();
+	});
+
 	document.querySelector("main").appendChild(note);
 };
 
-newNoteBtn.addEventListener("click", addNote);
+if (storedNotes) {
+	storedNotes.forEach((note) => addNote(note));
+}
+
+newNoteBtn.addEventListener("click", () => addNote());
+
+const updateLocalStorage = () => {
+	const notesText = document.querySelectorAll(".note-text");
+	const notes = [];
+
+	notesText.forEach((note) => notes.push(note.textContent.trim()));
+
+	localStorage.setItem("notes", JSON.stringify(notes));
+};
